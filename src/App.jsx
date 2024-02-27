3088c4a428706be78e9f43d09cce29846c3b5a0b
Agrey Tosira <agreytosira@gmail.com>
2024-02-27 12:07:10 +0700

    feat: test create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from './components/Navbar'
+
 function App() {
   return (
-    <main>
-      <h1 className='text-blue-500 text-2xl font-bold'>AimPocket</h1>
-    </main>
+    <>
+      <header>
+        <Navbar />
+      </header>
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className='p-4'>
+      <div className='container'>
+        <h1 className='font-bold text-blue-600 text-2xl'>AimPocket</h1>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
