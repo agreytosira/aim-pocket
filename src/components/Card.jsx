228b98f287d120720b0de40bc3c876fe16f63e3c
Agrey Tosira <agreytosira@gmail.com>
2024-02-27 21:15:14 +0700

    style: fill card with dummy data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,16 @@ import { Link } from 'react-router-dom'
 
 function Card() {
   return (
-    <Link className='border border-gray-300 rounded-lg p-4 pointer' to='/'>
-      <h3 className='text-xl font-semibold'>iPhone 11</h3>
-      <p className='text-slate-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, fugit.</p>
+    <Link className='border border-gray-300 rounded-lg p-4 pointer hover:border-blue-600 transition-all duration-300 ease' to='/'>
+      <img className='aspect-video object-cover rounded-md' src='https://cdn.eraspace.com/media/catalog/product/a/p/apple_iphone_11_black_new_1_7.jpg' alt='' />
+      <div className='bg-slate-300 w-full h-2 rounded-xl my-4'>
+        <div className='bg-blue-600 w-[75%] h-2 rounded-xl'></div>
+      </div>
+      <h3 className='text-lg text-slate-900 font-bold'>iPhone 11</h3>
+      <h4 className='text-base text-slate-600 font-semibold'>Rp7.950.000</h4>
+      <p className='text-[12px]'>
+        tersisa <span className='font-bold text-red-600'>Rp7.250.000</span>
+      </p>
     </Link>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Card from '../components/Card'
 function Home() {
   return (
     <div className='container mx-auto'>
-      <div className='grid grid-cols-4 gap-6'>
+      <div className='grid grid-cols-4 gap-4'>
         <Card />
         <Card />
         <Card />
