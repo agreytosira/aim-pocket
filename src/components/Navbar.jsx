75bdd63ece6972adc8ab6b89b8f3dcba7d323843
Agrey Tosira <agreytosira@gmail.com>
2024-02-27 12:17:40 +0700

    feat: set active color on navbar link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function Navbar() {
+  const location = useLocation().pathname
+
   return (
     <nav className='p-4'>
       <div className='container flex align-center justify-between'>
         <h1 className='font-bold text-blue-600 text-2xl'>AimPocket</h1>
         <ul className='flex gap-4'>
           <li>
-            <Link to='/' className='font-semibold'>
+            <Link to='/' className={`${location == '/' && 'text-blue-600'} font-semibold`}>
               Home
             </Link>
           </li>
           <li>
-            <Link to='/about' className='font-semibold'>
+            <Link to='/about' className={`${location == '/about' && 'text-blue-600'} font-semibold`}>
               About
             </Link>
           </li>
