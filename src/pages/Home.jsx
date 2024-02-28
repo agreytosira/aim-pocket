50b2d6c58d47d4eeff72d3e2a7b44276ee6adf91
Agrey Tosira <agreytosira@gmail.com>
2024-02-28 21:08:49 +0700

    feat: add floating add button in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Card from '../components/Card'
 import savings from '../data/dummy'
 
@@ -5,6 +6,9 @@ function Home() {
   return (
     <div className='container mx-auto'>
       <div className='grid grid-cols-4 gap-4'>{savings && savings.map((saving) => <Card id={saving.id} key={saving.id} isCompleted={saving.isCompleted} />)}</div>
+      <Link to='/add' className='fixed bottom-4 right-4 p-4 aspect-square bg-blue-600 text-white rounded-full w-12 h-12 flex items-center justify-center'>
+        +
+      </Link>
     </div>
   )
 }
