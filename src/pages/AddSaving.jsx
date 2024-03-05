bb5b1dfc08086a71bdf1c33bd0840100d0ad7048
Agrey Tosira <agreytosira@gmail.com>
2024-03-05 22:21:14 +0700

    style: improve AddSaving page

--- src/pages/AddSaving.jsx
+++ src/pages/AddSaving.jsx
@@ -28,25 +28,30 @@ const AddSaving = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col'>
-      <label>
-        Name:
-        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
-      </label>
-      <label>
-        Image URL:
-        <input type='text' value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
-      </label>
-      <label>
-        Target:
-        <input type='number' value={target} onChange={(e) => setTarget(e.target.value)} />
-      </label>
-      <label>
-        Nominal:
-        <input type='number' value={nominal} onChange={(e) => setNominal(e.target.value)} />
-      </label>
-      <button type='submit'>Add Saving</button>
-    </form>
+    <div className='container w-full sm:w-4/5 md:w-3/5 lg:w-2/6'>
+      <form onSubmit={handleSubmit} className='flex flex-col space-y-2'>
+        <h1 className='mb-4 text-2xl font-bold text-center'>Buat Tabungan Baru</h1>
+        <label className='flex flex-col'>
+          Nama Tabungan
+          <input className='px-2 py-1 border rounded-md outline-none border-slate-300 focus:border-blue-600' type='text' value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label className='flex flex-col'>
+          Link URL Gambar
+          <input className='px-2 py-1 border rounded-md outline-none border-slate-300 focus:border-blue-600' type='text' value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+        </label>
+        <label className='flex flex-col'>
+          Jumlah yang ingin dicapai
+          <input className='px-2 py-1 border rounded-md outline-none border-slate-300 focus:border-blue-600' type='number' value={target} onChange={(e) => setTarget(e.target.value)} />
+        </label>
+        <label className='flex flex-col'>
+          Nominal
+          <input className='px-2 py-1 border rounded-md outline-none border-slate-300 focus:border-blue-600' type='number' value={nominal} onChange={(e) => setNominal(e.target.value)} />
+        </label>
+        <button type='submit' className='py-2 mt-4 text-sm font-semibold text-white bg-blue-600 rounded-md'>
+          Buat Tabungan
+        </button>
+      </form>
+    </div>
   )
 }
 
