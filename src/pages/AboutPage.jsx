7447055d08d24209821a6bbf8fc8e781aa3a780b
Agrey Tosira <agreytosira@gmail.com>
2024-03-06 09:07:30 +0700

    feat: update about page content (temp)

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,12 @@
 function AboutPage() {
-  return <div>AboutPage</div>
+    return (
+        <div className='container w-full sm:w-4/5 md:w-3/5 lg:w-2/5 min-h-[80dvh]'>
+            <h1 className='text-2xl font-bold mb-2'>Apa itu AimPocket?</h1>
+            <p>
+                <strong>AimPocket</strong> adalah aplikasi web yang berfungsi untuk memudahkan pengguna dalam melacak tabungan dan membuat perencanaan.
+            </p>
+        </div>
+    );
 }
 
-export default AboutPage
+export default AboutPage;
