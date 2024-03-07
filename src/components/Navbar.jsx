8b6669fbe7685ca509a7207ad3c26ff61a207cfd
Agrey Tosira <agreytosira@gmail.com>
2024-03-07 11:19:21 +0700

    refactor: rename navbar link to bahasa indonesia

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,35 @@
-import { Link, useLocation } from 'react-router-dom';
-import ToggleTheme from './ToggleTheme';
+import { Link, useLocation } from 'react-router-dom'
+import ToggleTheme from './ToggleTheme'
 
 function Navbar() {
-    const location = useLocation().pathname;
+  const location = useLocation().pathname
 
-    return (
-        <nav className='py-4'>
-            <div className='container mx-auto'>
-                <div className='flex justify-between align-center'>
-                    <h1 className='text-2xl font-bold text-blue-600'>
-                        <Link to='/'>AimPocket</Link>
-                    </h1>
-                    <ul className='flex gap-6 items-center'>
-                        <li>
-                            <Link to='/' className={`${location == '/' && 'text-blue-600'} font-semibold`}>
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/about' className={`${location == '/about' && 'text-blue-600'} font-semibold`}>
-                                About
-                            </Link>
-                        </li>
-                        <li className='aspect-square size-3 flex items-center'>
-                            <ToggleTheme />
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className='py-4'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between align-center'>
+          <h1 className='text-2xl font-bold text-blue-600'>
+            <Link to='/'>AimPocket</Link>
+          </h1>
+          <ul className='flex items-center gap-6'>
+            <li>
+              <Link to='/' className={`${location == '/' && 'text-blue-600'} font-semibold`}>
+                Beranda
+              </Link>
+            </li>
+            <li>
+              <Link to='/about' className={`${location == '/about' && 'text-blue-600'} font-semibold`}>
+                Tentang
+              </Link>
+            </li>
+            <li className='flex items-center aspect-square size-3'>
+              <ToggleTheme />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar
