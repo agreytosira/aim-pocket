6135782601f5303efef39fd736d3aeaec70c45fd
Agrey Tosira <agreytosira@gmail.com>
2024-03-07 12:58:31 +0700

    style: hide action button text on mobile

--- src/components/DeleteSavingDataButton.jsx
+++ src/components/DeleteSavingDataButton.jsx
@@ -3,7 +3,7 @@ import { FaRegTrashCan } from 'react-icons/fa6'
 const DeleteSavingDataButton = ({ deleteHandler }) => {
   return (
     <button className='flex items-center gap-2 p-4 font-semibold text-white bg-red-600 rounded-lg ' onClick={() => deleteHandler()}>
-      <FaRegTrashCan /> Hapus
+      <FaRegTrashCan /> <p className='hidden sm:flex'>Hapus</p>
     </button>
   )
 }
--- src/components/IncreaseButton.jsx
+++ src/components/IncreaseButton.jsx
@@ -3,7 +3,7 @@ import { FaPlus } from 'react-icons/fa6'
 const IncreaseButton = ({ increaseHandler }) => {
   return (
     <button className='flex items-center gap-2 p-4 font-semibold text-white bg-blue-600 rounded-lg' onClick={() => increaseHandler()}>
-      <FaPlus /> Tambah
+      <FaPlus /> <p className='hidden sm:flex'>Tambah</p>
     </button>
   )
 }
