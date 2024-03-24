ae152c3fd24639e244fa590b56a17b4a13793de5
Agrey Tosira <agreytosira@gmail.com>
2024-03-24 14:13:35 +0700

    fix: action button text color

--- src/components/DecreaseButton.jsx
+++ src/components/DecreaseButton.jsx
@@ -3,7 +3,7 @@ import { FaMinus } from 'react-icons/fa6'
 const DecreaseButton = ({ decreaseHandler }) => {
   return (
     <button className='flex items-center gap-2 p-4 font-semibold text-white bg-orange-600 rounded-lg' onClick={() => decreaseHandler()}>
-      <FaMinus /> <p className='hidden sm:flex'>Kurang</p>
+      <FaMinus /> <p className='hidden text-white sm:flex'>Kurang</p>
     </button>
   )
 }
--- src/components/IncreaseButton.jsx
+++ src/components/IncreaseButton.jsx
@@ -3,7 +3,7 @@ import { FaPlus } from 'react-icons/fa6'
 const IncreaseButton = ({ increaseHandler }) => {
   return (
     <button className='flex items-center gap-2 p-4 font-semibold text-white bg-blue-600 rounded-lg' onClick={() => increaseHandler()}>
-      <FaPlus /> <p className='hidden sm:flex'>Tambah</p>
+      <FaPlus /> <p className='hidden text-white sm:flex'>Tambah</p>
     </button>
   )
 }
